ea3d10421cf3c7e9bbcfb3b9689798dbf586e1c2
clotagat3u <clotagat3u@mim-b213-07.ad.univ-lorraine.fr>
2018-05-14 15:16:55 +0200

    Version 0.27

--- HeatsthStone/src/Capacité/Capacité.java
+++ HeatsthStone/src/Capacité/Capacité.java
@@ -1,13 +1,11 @@
 package Capacité;
 
-public interface Capacité {
-	
+public interface Capacité {	
 	void executerAction(Object cible);
 	void executerEffetDebutTour();
 	void executerEffetDisparition(Object cible);
 	void executerEffetFinTour();
 	void executerEffetMiseEnJeu(Object cible);
 	String getDescription();
-	String getNom(); 
-
+	String getNom();  
 }
