2c2a1976e26f90e83748cda39f15ccd21c49e1b4
clotagat3u <clotagat3u@mim-b203-07.ad.univ-lorraine.fr>
2018-05-22 13:15:15 +0200

    Version 4.41 (vérif)

--- HeatsthStone/src/capacites/TirAssure.java
+++ HeatsthStone/src/capacites/TirAssure.java
@@ -7,7 +7,7 @@ public class TirAssure extends Capacite {
 	public TirAssure() {
 		super("Tir assuré", "Pouvoir héroique: inflige 2 point de dégât au héros adverse");
 	}
-	
+
 	@Override
 	public void executerAction(Object cible) {		
 		super.executerAction(cible);
