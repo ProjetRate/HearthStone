3eb4ab13ef0b46713885c5bf519a913d7a2ba90b
Nathan <Nathan@pc-de-blabla>
2018-06-05 03:43:43 +0200

    Version 7.0 (Javadoc)

---
+++ HeatsthStone/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"capacite"},{"l":"capacites"},{"l":"carte"},{"l":"deck"},{"l":"exception"},{"l":"heros"},{"l":"joueur"},{"l":"main"},{"l":"plateau"},{"l":"serviteurs"},{"l":"sorts"}]
